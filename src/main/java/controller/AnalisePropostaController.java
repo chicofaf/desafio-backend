9d708b614a1c8f50552b30b525e205619750d7d5
chicofaf <chicofaf@gmail.com>
2019-01-30 09:25:22 -0200

    Update AnalisePropostaController.java
    ajuste analisar proposta

--- src/main/java/controller/AnalisePropostaController.java
+++ src/main/java/controller/AnalisePropostaController.java
@@ -3,12 +3,43 @@ package controller;
 import dao.FactoryDao;
 import model.Proposta;
 
-public class AnalisePropostaController {
-	public void analisarProposta(Proposta proposta) {
-		if (proposta.getRenda() < 1000) {
+public void analisarProposta(Proposta proposta) {
+		if (proposta.getRenda() <= 800) {
 			proposta.setResultadoAnalise("Negado");
 			proposta.setLimite("renda baixa");
 		}
+		if (proposta.getRenda() > 800 && proposta.getRenda() <= 2500) {			
+			if(proposta.getDependentes() > 0){			
+				proposta.setResultadoAnalise("Negado");
+				proposta.setLimite("reprovado pela política de crédito");		
+			} else {
+				proposta.setResultadoAnalise("Aprovado");
+				proposta.setLimite("entre 500 - 1000");		
+			}
+		}
+		if (proposta.getRenda() > 2500 && proposta.getRenda() <= 5000) {
+			if(proposta.getDependentes() > 0){
+				int rendaPorDependente = proposta.getRenda()/proposta.getDependentes();
+				if(rendaPorDependente < 1000) {
+					proposta.setResultadoAnalise("Negado");
+					proposta.setLimite("reprovado pela política de crédito");	
+				} else {
+					proposta.setResultadoAnalise("Aprovado");
+					proposta.setLimite("entre 1000 - 1500");		
+				}
+			}
+		}
+		if (proposta.getRenda() > 5000 && proposta.getRenda() <= 10000) {
+			if(proposta.getDependentes() > 0){
+				int rendaPorDependente = proposta.getRenda()/proposta.getDependentes();
+				if(rendaPorDependente < 1000) {
+					proposta.setResultadoAnalise("Negado");
+					proposta.setLimite("reprovado pela política de crédito");	
+				} else {
+					proposta.setResultadoAnalise("Aprovado");
+					proposta.setLimite("superior 2000");		
+				}
+			}
+		}
 		FactoryDao.getPropostaDAO().create(proposta);
 	}
-}
